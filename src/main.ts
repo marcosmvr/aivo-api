422c47d276e032c7386f90bc83dd919176c2c1b1
Marcos <marcosviniciusreis260@gmail.com>
2026-02-09 21:47:05 -0300

    chore: add origin true

--- src/main.ts
+++ src/main.ts
@@ -16,12 +16,7 @@ async function bootstrap() {
   )
 
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'http://localhost:3000',
-      /\.lovable\.app$/,
-      /\.vercel\.app$/,
-    ],
+    origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   })
